9d285c71b555daee3613dce6a4dbca203e993177
Galileu Batista <gbatmobile@gmail.com>
2023-09-09 13:43:26 -0300

    Add support to filter files in date range

--- photorg.py
+++ photorg.py
@@ -23,8 +23,9 @@ onlyPhotos = False
 importEvents = False
 eventFile = None
 events = {}
+dateRange = []
 eventSeparator = "--"
-    
+
 def locateAllFiles(dirs):
     global events
     log ("STATUS: Locating files in "+str(dirs)+" ...")
@@ -51,15 +52,15 @@ def addType(allFiles):
             file['type'] = 'other'
     if onlyPhotos:
         log ("STATUS: Preserving only photos ...")
-        allFiles = [file for file in allFiles if file['type'] != 'other']        
-                
+        allFiles = [file for file in allFiles if file['type'] != 'other']
+
 def addDate(allFiles):
     log ("STATUS: Searching for the best date for each file ...")
     now = datetime.datetime.now()
     for file in allFiles:
         fileName = file['name']
         dt = now
-        
+
         try:
             header = piexif.load(fileName)
             exifdt = header["0th"][piexif.ImageIFD.DateTime].decode()
@@ -77,31 +78,40 @@ def addDate(allFiles):
                     pass
         file['date'] = dt
 
+def filterDateRange(allFiles):
+    if len(dateRange) == 2:
+        log ("STATUS: Filtering files in date range ...")
+        allFiles = list(filter(lambda file: dateRange[0] < file['date']
+                                                         < dateRange[1],
+                               allFiles))
+    return allFiles
+
 def calcSHA256(fileName):
         fd = open(fileName,  "rb")
         m = hashlib.sha256()
         m.update(fd.read())
         fd.close()
         return binascii.hexlify(m.digest()).decode()
-       
+
 def checkForDuplicates(allFiles):
     log ("STATUS: Identifying duplicated files in source dirs ...")
     for file in allFiles:
         file ['dupFile'] = None
-        
-    for i in range(len(allFiles)):
-        for j in range(i+1,  len(allFiles)):
-            if (not allFiles[j]['dupFile'] and
-                allFiles[i]['size'] == allFiles[j]['size']):
-                if not allFiles[i].get('sha256', None):
-                    allFiles[i]['sha256'] = calcSHA256(allFiles[i]['name'])
-                if not allFiles[j].get('sha256', None):
-                    allFiles[j]['sha256'] = calcSHA256(allFiles[j]['name'])
 
-                if (allFiles[i]['sha256'] == allFiles[j]['sha256']):
-                    allFiles[j]['dupFile'] = allFiles[i]['name']
-                    log("WARN: Duplicated in source: "+allFiles[j]['name']+" --> "+\
-                        allFiles[j]['dupFile'])
+    for i in range(len(allFiles)):
+        if not allFiles[i]['dupFile']:
+            for j in range(i+1,  len(allFiles)):
+                if (not allFiles[j]['dupFile'] and
+                    allFiles[i]['size'] == allFiles[j]['size']):
+                    if not allFiles[i].get('sha256', None):
+                        allFiles[i]['sha256'] = calcSHA256(allFiles[i]['name'])
+                    if not allFiles[j].get('sha256', None):
+                        allFiles[j]['sha256'] = calcSHA256(allFiles[j]['name'])
+
+                    if (allFiles[i]['sha256'] == allFiles[j]['sha256']):
+                        allFiles[j]['dupFile'] = allFiles[i]['name']
+                        log("WARN: Duplicated in source: "+allFiles[j]['name']+" --> "+\
+                            allFiles[j]['dupFile'])
 
 
 def checkForDupInTarget(allFilesSrc, allFilesTarget):
@@ -114,7 +124,7 @@ def checkForDupInTarget(allFilesSrc, allFilesTarget):
                     fileSrc['sha256'] = calcSHA256(fileSrc['name'])
                 if not fileTarget.get('sha256', None):
                     fileTarget['sha256'] = calcSHA256(fileTarget['name'])
-                    
+
                 if fileSrc['sha256'] == fileTarget['sha256']:
                     fileSrc['dupFile'] = fileTarget['name']
                     log("WARN: Duplicated in output: "+fileSrc['name']+" --> "+\
@@ -138,24 +148,24 @@ def dirNameFromDate(date):
     fullDirNameEvents = ""
     for dirName in fullDirName.split('/'):
         fullDirNameEvents += dirName + events.get(dirName, '') + '/'
-    
+
     return fullDirNameEvents
-    
+
 def getNameWithoutConfilct(targetDir, file):
     filename = os.path.basename(file['name'])
     targetFile = targetDir+"/"+filename
     while os.path.exists(targetFile):
         if not file.get('sha256', None):
             file['sha256'] = calcSHA256(file['name'])
-        if (calcSHA256(targetFile) == file['sha256'] and 
+        if (calcSHA256(targetFile) == file['sha256'] and
             os.stat(targetFile).st_size == file['size']):
             return None
         ext = os.path.splitext(targetFile)[1]
         if ext =='':  ext = ".ext"
         targetFile += ext
-        
+
     return targetFile
-    
+
 def copyFiles(outdir,  allFilesSrc, allFilesTarget):
     log ("STATUS: Copying files")
     copied = 0
@@ -165,8 +175,8 @@ def copyFiles(outdir,  allFilesSrc, allFilesTarget):
 
             if not simMode and not os.path.exists(targetDir):
                 os.makedirs(targetDir)
-        
-            targetFile =  getNameWithoutConfilct(targetDir, file) 
+
+            targetFile =  getNameWithoutConfilct(targetDir, file)
             if targetFile != None:
                 if  os.path.basename(targetFile) == os.path.basename(file['name']):
                     log ("COPY: "+file['name']+ ' to '+targetFile)
@@ -177,7 +187,7 @@ def copyFiles(outdir,  allFilesSrc, allFilesTarget):
                 copied += 1
             else:
                 file['dupFile'] = targetFile
-                log("WARN: Duplicated in destination dir: "+file['name']+" not copied!")                    
+                log("WARN: Duplicated in destination dir: "+file['name']+" not copied!")
     if simMode:
         log ("STATUS: Number of files should be copied: "+str(copied))
     else:
@@ -197,7 +207,7 @@ def deleteDupFiles(allFiles):
         log ("STATUS: Number of files should be deleted: "+str(deleted))
     else:
         log ("STATUS: Number of files deleted: "+str(deleted))
-    
+
 def log(msg):
     print (msg)
 
@@ -209,7 +219,7 @@ def importEventsFromDir(dir):
             if len(dirName) == 2:
                 events[dirName[0]] = eventSeparator+dirName[1]
     return events
-                    
+
 def addEventsFromFile(eventFile):
     log ("STATUS: Reading events from file "+eventFile+" ...")
     global events
@@ -219,18 +229,18 @@ def addEventsFromFile(eventFile):
         indSep = event.find(' ')
         if indSep > 0:
             events[event[:indSep]] = '--'+event[indSep+1:].strip()
-        
+
     fdEvent.close()
 
 def showHelpAndExit(msg=''):
 	showHelp(msg)
 	sys.exit(1)
-	
+
 def showHelp(msg=''):
     print ("photorg - An small photo organizer (v0.7).")
     print ("\t  by Galileu Batista - galileu.batista at ifrn.edu.br")
     print ("\t  contribution: Rodrigo Tavora")
-        
+
     print ("usage: "+sys.argv[0]+" [--option | -option param] ")
     print ("\t--help  - Show this help")
     print ("\t--usage - Show examples of usage\n")
@@ -245,6 +255,7 @@ def showHelp(msg=''):
     print ("\t--R - Only delete duplicated files in source dirs. No copy at all")
     print ("\t--i - Identify events in target-dir")
     print ("\t      if output folder has an event name, it will be used")
+    print ("\t-s YYYY-MM-DD YYYY-MM-DD - organize file only in date range")
     print ("\t-e event-file - a file with descriptions for events on dates")
     print ("\t                event names will be appended to folder name")
     print ("\t\tformat and example (do not use -- in event names):")
@@ -255,9 +266,9 @@ def showHelp(msg=''):
     print ("\t-d source-dir - Where files will be copied from (default: .)")
     print ("\t                Can be used multiple times.")
     print ("\t-o target-dir - Where files will be copied to (default: ./classified)")
-    
+
     print (msg)
-    
+
 
 def showExamplesAndExit():
     print ("Usage examples....")
@@ -266,29 +277,29 @@ def showExamplesAndExit():
     print ("\n{} --y --n -d c:/unclass -o c:/class".format(sys.argv[0]))
     print ("\tsimululate organization from c:/unclass to c:/class - year basis")
     print ("\tbecause --n, **no copies and/or deletions are done** at all.")
-    print ("\n{} --m -l en -d c:/unclass -o c:/class".format(sys.argv[0]))
-    print ("\tCopy files from c:/unclass to c:/class - month basis")
+    print ("\n{} --m -l en -d 'c:/unclass' -o 'c:/class';".format(sys.argv[0]))
+    print ("\tCopy files from 'c:/unclass' to 'c:/class' - month basis")
     print ("\tuse month names in english (locate: -l en).")
-    print ("\n{} --r --m -d c:/unclass -o c:/class".format(sys.argv[0]))
-    print ("\tCopy files to c:/class and **remove duplicates** in c:/unclass")
+    print ("\n{} --r --m -d 'c:/unclass' -o 'c:/class';".format(sys.argv[0]))
+    print ("\tCopy files to 'c:/class' and **remove duplicates** in 'c:/unclass';")
     print ("\tduplicates are checked (size,sha256) in source dirs.")
     print ("\n{} --c --r --m -d c:/unclass -o c:/class".format(sys.argv[0]))
-    print ("\tCopy files to c:/class and **remove duplicates** in c:/unclass")
-    print ("\tduplicates are checked (size,sha256) against sources and target dirs.")
+    print ("\tCopy files to c:/class and **remove duplicates** in 'c:/unclass';")
+    print ("\tDuplicates are checked (size,sha256) against sources and target dirs.")
     print ("\n{} --c --R --m -d c:/unclass -o c:/class".format(sys.argv[0]))
     print ("\t**No copy** any files (--R), but **remove duplicates** in source dirs")
-    print ("\tduplicates are checked (size,sha256) against source and target dirs.")
+    print ("\tDuplicates are checked (size,sha256) against source and target dirs.")
     print ("\n{} --c --r --p --m -d c:/unclass -o c:/class".format(sys.argv[0]))
     print ("\tCopy **only pictures** to c:/class and **remove duplicates** in source")
-    print ("\tduplicates are checked (size,sha256) against source and target dirs.")
-    
+    print ("\tDuplicates are checked (size,sha256) against source and target dirs.")
+
     sys.exit(0)
-    
+
 def processOptions():
-    global  organizeBy, importEvents, eventFile, sourceDir
+    global  organizeBy, importEvents, eventFile, sourceDir, dateRange
     global  targetDir, fullChkDup, simMode, delDup, onlyDelDup, onlyPhotos
-    
-    if len(sys.argv) < 2: 
+
+    if len(sys.argv) < 2:
         showHelpAndExit()
 
     sourceDir = []
@@ -299,8 +310,8 @@ def processOptions():
     delDup = False
     onlyDelDup = False
     locale.setlocale(locale.LC_ALL, "pt")
-    
-    try:        
+
+    try:
         nextArg = 1
         while nextArg < len(sys.argv):
             if sys.argv[nextArg] in organizeOptions:
@@ -317,6 +328,12 @@ def processOptions():
                 importEvents = True
             elif sys.argv[nextArg] == '--p':
                 onlyPhotos = True
+            elif sys.argv[nextArg] == '-s' and (nextArg+2) < len(sys.argv):
+                dateRange = []
+                dateRange.append(datetime.datetime.strptime(sys.argv[nextArg+1], "%Y-%m-%d"))
+                dateRange.append(datetime.datetime.strptime(sys.argv[nextArg+2], "%Y-%m-%d"))
+                dateRange[1] += datetime.timedelta(seconds=86399)
+                nextArg += 2
             elif sys.argv[nextArg] == '-e' and (nextArg+1) < len(sys.argv):
                 eventFile = sys.argv[nextArg+1]
                 nextArg += 1
@@ -340,10 +357,10 @@ def processOptions():
 
         if len(sourceDir) == 0:
             sourceDir = ["."]
-            
+
         if targetDir == None:
             targetDir = "./classified"
-            
+
         log ("Option: '{}' SourceDir: '{}' TargetDir: '{}' Locale: {}".format(
              organizeBy, sourceDir, targetDir, locale.getlocale()))
     except Exception as e:
@@ -364,13 +381,14 @@ if __name__ == "__main__":
     if eventFile:
         addEventsFromFile(eventFile)
 
-    allFilesSrc = locateAllFiles(sourceDir)        
+    allFilesSrc = locateAllFiles(sourceDir)
     addType(allFilesSrc)
     addDate(allFilesSrc)
+    allFilesSrc = filterDateRange(allFilesSrc)
     checkForDuplicates(allFilesSrc)
 
     if fullChkDup:
-        allFilesTarget = locateAllFiles([targetDir])        
+        allFilesTarget = locateAllFiles([targetDir])
         checkForDupInTarget(allFilesSrc, allFilesTarget)
     else:
         allFilesTarget = []
@@ -381,6 +399,6 @@ if __name__ == "__main__":
         copyFiles(targetDir,  allFilesSrc, allFilesTarget)
         if delDup:
             deleteDupFiles(allFilesSrc)
-        
+
     log ("STATUS: Processing finished!!!!")
 
